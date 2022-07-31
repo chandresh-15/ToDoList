5ec7eb8597f1a85c6bb8131f252cba609bd0f398
chandresh <chandreshsachala1@gmail.com>
2022-07-31 13:22:18 +0530

    edit check task

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
 var taskele = document.querySelector("#task-input-text");
 var taskdata = document.querySelector(".task-box");
-var count=0;
-function addtask(){
-    if(taskele.value == "")
-    {
-        alert("pleasr enter your task");
-    }
-    else{
-        console.log(taskele.value);
-        count++;
-        var newul =`<li class="task" id="${count}"><input id="ontick" type="CheckBox" onclick="checkit(${count})">${taskele.value}<button class="delfunction" type="button" onclick="deleteTask(${count})"><i class="uil uil-trash"></i>delete</button></li><br>`
-        taskdata.innerHTML += newul;
-        taskele.value = "";
-    }
+var count = 0;
+function addtask() {
+  if (taskele.value == "") {
+    alert("pleasr enter your task");
+  } else {
+    console.log(taskele.value);
+    count++;
+    var newul = `<li class="task" id="${count}"><input id="ontick" type="CheckBox" onclick="checkit(${count})">${taskele.value}<button class="delfunction" type="button" onclick="deleteTask(${count})"><i class="uil uil-trash"></i>delete</button></li><br>`;
+    taskdata.innerHTML += newul;
+    taskele.value = "";
+  }
 }
-function deleteTask(taskid){
-    var state=confirm("Are you sure want to delete this product ? ");
-    if(state)
-        document.getElementById(taskid).remove();
+function deleteTask(taskid) {
+  var state = confirm("Are you sure want to delete this product ? ");
+  if (state) document.getElementById(taskid).remove();
 }
 
-function checkit(taskid){
-        document.getElementById(taskid).style.textDecoration = "line-through"; 
-        document.getElementById(taskid).style.backgroundColor = "green"; 
+function checkit(taskid) {
+  if (sign == true) {
+    document.getElementById(taskid).style.textDecoration = "line-through";
+    document.getElementById(taskid).style.backgroundColor = "green";
+    sign = !sign;
+  } else {
+    document.getElementById(taskid).style.textDecoration = "";
+    document.getElementById(taskid).style.backgroundColor = "";
+    sign = !sign;
+  }
 }
